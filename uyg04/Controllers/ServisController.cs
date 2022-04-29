544ed22813ccd0d6d8c87a0862d29ffccefd1437
ender0b <73099860+ender0b@users.noreply.github.com>
2022-04-29 21:53:11 +0300

    Araç kiralama portalı
    İnternet Programcılığı 2 Vize Proje Ödevi

---
+++ uyg04/Controllers/ServisController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace uyg04.Controllers
+{
+    public class ServisController : ApiController
+    {
+    }
+}
